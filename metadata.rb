93410097612c28c7b2c6c30218cafe4093d2d18c
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-06-22 16:47:43 -0400

    Update chef_version pin
    We should continue to work with recent Chef 12 versions.

--- metadata.rb
+++ metadata.rb
@@ -5,12 +5,12 @@ license          'Apache-2.0'
 description      'Installs/Configures storage'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '6.0.2'
+chef_version     '>= 12.11', '< 15.0.0'
 
 source_url       'https://github.com/darkskyapp/storage-cookbook'
 issues_url       'https://github.com/darkskyapp/storage-cookbook/issues'
 
 supports 'ubuntu', '>= 16.04'
-chef_version '~> 12.10'
 
 depends 'ohai'
 depends 'aws', '~> 4.0'
